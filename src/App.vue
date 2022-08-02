922e0605fd8384247e625d5033ba3c4605f2e9de
Tiago Mota <tiagodiggermota@hotmail.com>
2022-08-02 14:58:14 -0300

    Iniciando as funcionalidades de start e stop do temporizador

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
     </div>
     <div class="column is-three-quarter">
       <Formulario />
+      <!-- Lista de tarefas -->
     </div>
   </main>
 </template>
--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -15,14 +15,14 @@
             <div class="column">
                <div class="is-flex is-align-items-center is-justify-content-space-between">
                 <section>
-                    <strong>00:00:00</strong>
+                    <strong> {{ tempoDecorrido }}</strong>
                 </section>
-                <button class="button">
+                <button class="button" @click="iniciar">
                     <span class="icon">
                         <i class="fas fa-play"></i>
                     </span>
                 </button>
-                <button class="button">
+                <button class="button" @click="finalizar">
                     <span class="icon">
                         <i class="fas fa-stop"></i>
                     </span>
@@ -37,7 +37,28 @@
    import { defineComponent } from 'vue'
 
     export default defineComponent({
-        name:'Formulário'
-    }) 
+        name:'Formulário',
+        data (){
+            return{
+                tempoEmSegundos:0,
+                cronometro: 0
+            }
+        },
+        computed: {
+            tempoDecorrido () : string {
+                return new Date(this.tempoEmSegundos * 1000).toISOString().substr(11,8)
+            }
+        },
+        methods:{
+            iniciar () {
+                this.cronometro = setInterval(() => {
+                    this.tempoEmSegundos += 1
+                }, 1000)
+            },
+            finalizar () {
+                clearInterval(this.cronometro)
+            }
+        }
+    }); 
 
 </script>
